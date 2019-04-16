43cdd48d26e1a3cc23ad3b9824222e908ef5f945
NARONGPON.SU <narongpon.su@glsict.com>
2019-04-16 12:35:32 +0700

    add lodash

--- src/controller/PrioritizationController.ts
+++ src/controller/PrioritizationController.ts
@@ -3,6 +3,8 @@ import { getCustomRepository } from 'typeorm'
 import { CriteriaRepository } from '../repository/CritetiaRepository'
 import { PrioritizationRepository } from '../repository/PrioritizationRepository'
 import { RiskAssessmentIndicatorRepository } from '../repository/RiskAssessmentIndicatorRepository';
+import * as _ from 'lodash'
+import { PerformanceMeasurementRepository } from '../repository/PerformanceMeasurementRepository';
 
 const create = async (req: Request, res: Response) => {
 	try {
@@ -15,15 +17,29 @@ const create = async (req: Request, res: Response) => {
 			await riskIndicatorRepo.save(riskIndicator)
 			for (let score of item.prioritization_score) {
 				const prioritization = prioritizationRepo.create()
-				prioritization.id = score.prioritization_id
-				prioritization.criteria_id = score.criteria_id
-				prioritization.risk_assessment_indicator_id = item.risk_assessment_indicator_id
-				prioritization.score = score.score
+				if (score.prioritization_id) {
+					prioritization.id = score.prioritization_id
+					prioritization.score = score.score
+				} else {
+					prioritization.criteria_id = score.criteria_id
+					prioritization.risk_assessment_indicator_id = item.risk_assessment_indicator_id
+					prioritization.score = score.score
+				}
 				await prioritizationRepo.save(prioritization)
 			}
 		}
+
+		if (!req.body.isDraft) {
+			const maxPiority = _.maxBy(req.body.prioritization, 'priority_score')
+			const perfMeasRepo = getCustomRepository(PerformanceMeasurementRepository)
+			const perfMeas = perfMeasRepo.create()
+			perfMeas.risk_assessment_indicator = maxPiority.risk_assessment_indicator_id
+			await perfMeasRepo.save(perfMeas)
+		}
+
 		return res.status(201).json({ result: true })
 	} catch (e) {
+		console.log(e)
 		return res.status(500).json({ e })
 	}
 }
@@ -43,7 +59,6 @@ const getPrioritization = async (req: Request, res: Response) => {
 	try {
 		const repo = getCustomRepository(PrioritizationRepository)
 		const result = await repo.getPrioritization(req.query.org_id, req.query.year)
-		console.log(result)
 		return res.status(200).json({ result })
 	} catch (e) {
 		return res.status(500).json({ e })
